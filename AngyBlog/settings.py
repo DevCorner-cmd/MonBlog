718a1548ab8c08ea6687db952b1951ba7081adf0
Jean <eservicetaabo@gmail.com>
2026-03-01 22:22:09 +0000

    mise à jour settings

--- AngyBlog/settings.py
+++ AngyBlog/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['angyblog.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
